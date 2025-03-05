91f51b267623cc2604c24041a191e4315dfc2109
Anton Kuzmenko <anton.kuzmenko2014@mail.ru>
2025-03-05 03:28:41 +0300

    Спринт-1: Вывести карточки на страницу, добавить функцию удаления карточки

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/m1800555/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
+function createCard(cardData, deleteFn) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImage = card.querySelector('.card__image');
+  cardImage.setAttribute('src', cardData.link);
+  cardImage.setAttribute('alt', cardData.name);
 
-// @todo: Вывести карточки на страницу
+  card.querySelector('.card__title').textContent = cardData.name;
+
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteFn);
+  deleteButton.targetElement = card;
+
+  return card;
+}
+
+// Функция удаления карточки
+function deleteCard(event) {
+  event.currentTarget.targetElement.remove();
+}
+
+// Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  card = createCard(cardData, deleteCard);
+  placesList.append(card);
+});
+
